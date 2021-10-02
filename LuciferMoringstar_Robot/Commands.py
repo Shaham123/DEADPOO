b7bcd9e478b5575cf86349cc500dc82f9f41cbee
Shaham123 <89772416+Shaham123@users.noreply.github.com>
2021-10-02 20:34:14 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -112,7 +112,7 @@ async def start(bot, message):
             )
         )
     else:
-        await cmd.reply_video(
+        await .reply_video(
             video="https://telegra.ph/file/2b81aea93b9df8d3d4df3.mp4",
             caption=START_MSG,
             reply_markup=InlineKeyboardMarkup(
