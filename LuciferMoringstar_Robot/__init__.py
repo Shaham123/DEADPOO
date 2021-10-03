270ab5cac93810473ba3f89376e627d05104ec22
Shaham123 <89772416+Shaham123@users.noreply.github.com>
2021-10-03 09:25:07 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -25,7 +25,7 @@ Bot Owner Only
 /logger  Get Logs
 /delete Delete File From Database
 
-[➡️How To Add Old Files For database](https://youtu.be/mtu5hgW-l_o)
+Don't spam me 😬
 
 @malllumoviesgroups
 """
